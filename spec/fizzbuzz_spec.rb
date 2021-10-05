5357b7dcf3b069ceb3e5191dd98f694c2664b8c4
Myoung <mhbae93@gmail.com>
2021-10-05 19:21:51 +0900

    rspec test cases and fizzbuzz updated

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,22 @@
-require "./lib/fizzbuzz"
+require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "number" when passed other' do
+    expect(fizzbuzz(13)).to eq 13
   end
 end
